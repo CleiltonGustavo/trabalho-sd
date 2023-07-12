c094f76e52058749e3184c5e359228980a0ed804
VictorVeras7 <105512452+VictorVeras7@users.noreply.github.com>
2023-07-11 23:50:33 -0300

    Update clientejogador.py

--- jogo/clientejogador.py
+++ jogo/clientejogador.py
@@ -3,7 +3,7 @@ import json
 
 # Configurações do cliente
 HOST = 'localhost'  # Pode ser o IP do seu servidor
-PORT = 12345  # Porta do servidor
+PORT = 4000  # Porta do servidor
 
 # Cria um socket TCP/IP
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,9 +18,8 @@ if flag == '1'or 1:
     sua_vez = True
 else:
     sua_vez = False    
-while True:    
-
-    if sua_vez == True:
+while True:
+    if sua_vez:
         req = client_socket.recv(1024)
 
         if not req:
@@ -28,24 +27,33 @@ while True:
             print('Servidor desconectado.')
             break
 
-        sua_vez = False
         # Decodifica a mensagem recebida
         mensagem = req.decode('utf-8')
 
         # Exibe a mensagem de solicitação do servidor
         print(mensagem)
-        # Prepara os dados da jogada
-        linha = int(input('Digite a linha: '))
-        coluna = int(input('Digite a coluna: '))
-        jogada = {'linha': linha, 'coluna': coluna}
 
+        while True:
+            # Prepara os dados da jogada
+            linha = input('Digite a linha: ')
+            coluna = input('Digite a coluna: ')
+
+            if linha.isdigit() and coluna.isdigit():
+                linha = int(linha)
+                coluna = int(coluna)
+
+                if linha in [0, 1, 2] and coluna in [0, 1, 2]:
+                    break
+            print('Valores inválidos! A linha e a coluna devem ser 0, 1 ou 2.')
+
+        jogada = {'linha': linha, 'coluna': coluna}
 
         # Converte a jogada em JSON
         message = json.dumps(jogada)
-        
 
         # Envia a mensagem para o servidor
         client_socket.sendall(message.encode('utf-8'))
+        sua_vez = False
     else:
         sua_vez = True
 
